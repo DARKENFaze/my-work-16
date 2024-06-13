711c87afc2cb60adeca5fb2e4f1cdda8a2ccb817
DARKEN-Ae <arslantrad897@gmail.com>
2024-06-13 19:19:12 +0500

    edit index

--- js/main.js
+++ js/main.js
@@ -31,9 +31,31 @@ let modeBtn = document.getElementById("mode-btn");
 
 modeBtn.addEventListener("click", function () {
   if (document.body.className != "dark") {
-    this.firstElementChild.src = "./images/header/sun.svg";
+    this.firstElementChild.src = "./images/more-images/moon.svg";
   } else {
-    this.firstElementChild.src = "./images/header/moon.svg";
+    this.firstElementChild.src = "./images/more-images/sun.svg";
   }
   document.body.classList.toggle("dark");
 });
+
+// Get the button
+let mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
